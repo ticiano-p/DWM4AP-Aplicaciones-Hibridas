cc63f5fad243651c0eb154d33227018f6afa2398
Ticiano Piccino <piccinoticiano@gmail.com>
2025-03-21 18:18:38 -0300

    Eliminar la carpeta nueva-carpeta

--- actividad4/index.js
+++
@@ -1,9 +0,0 @@
-// importamos el módulo
-const {ProductManger } = require('./ProductManger.js');
-const admin = new ProductManger()
-const id = crypto.randomUUID();
-
-admin.addProduct({id, name: 'Celu', price:2344});
-const json = admin.getProducts();
-console.log("📦 Productos almacenados:", JSON.stringify(json, null, 2));
-
